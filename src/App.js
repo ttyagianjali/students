5966240225348b63bd68bb83327ed6ec7dd0aab3
Anjali Tyagi <anjalityagi988@gmail.com>
2021-10-17 23:46:31 -0400

    fethced data from student api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+class App extends React.Component {
+  // Constructor
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      items: [],
+      isLoaded: false,
+    };
+  }
+
+  // ComponentDidMount is used to
+  // execute the code
+  componentDidMount() {
+    fetch("https://api.hatchways.io/assessment/students")
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({
+          items: res.students,
+          isLoaded: true,
+        });
+      });
+  }
+  render() {
+    const { isLoaded, items } = this.state;
+    if (!isLoaded)
+      return (
+        <div>
+          <h1> Please wait some time.... </h1>{" "}
+        </div>
+      );
+
+    return (
+      <div className="App">
+        {items.map((item) => (
+          <ol key={item.id}>
+            <img className="img" src={item.pic}></img> <br></br>
+            <div>
+              <div>
+                {item.firstName} {item.lastName}
+              </div>  
+              Email: {item.email} <br></br>
+              Company: {item.company} <br></br>
+              Skills: {item.skill}
+              <br></br>
+              Average:{" "}
+              {item.grades.reduce((a, b) => a + parseInt(b), 0) /
+                item.grades.length}
+            </div>
+            
+          </ol>
+          
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
